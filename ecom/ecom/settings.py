b0589b604dcd3a389ffb411f2da9c55c2edb06a9
antony-praveen <antony15898@gmail.com>
2021-07-14 13:06:04 +0530

    for live server change

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$d1ebcb72b)#rqdm7crq$*8&k5&mye(+dz0s0p-g+^w0k+(*dk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from shop import views
 from django.views.generic import TemplateView
 
+from django.views.static import serve
+from django.conf.urls import url
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="adminw"),
@@ -26,4 +29,8 @@ urlpatterns = [
     path("about/", TemplateView.as_view(template_name="shop/about.html"), name="about"),
     path('<int:obj_id>',views.detail, name="detail"),
     path("checkout/",views.checkout, name="checkout"),
+
+    
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
