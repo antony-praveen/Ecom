68681ecceb3068e390ff0c3a3f8763cba815203b
antony-praveen <antony15898@gmail.com>
2021-07-14 13:29:12 +0530

    allowed host setting change for live deploy

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -34,3 +34,4 @@ urlpatterns = [
     #url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
